43913cd22f078dc17d1d0de1847a31ffb87ba041
justindpurdie <justindpurdie@gmail.com>
2020-01-31 13:30:14 -0500

    auth

--- src/routes/secure.ts
+++ src/routes/secure.ts
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
-import  { default as PingController } from '../controllers/ping-controller'
+import  { default as PingController } from '../controllers/PingController'
+import  { default as OrgsController } from '../controllers/OrgsController'
 
 router.get('/ping', PingController.ping)
+
+// auth controllers
+
+router.post('/v1/organizations', OrgsController.validate('register'),  OrgsController.register)
 
 module.exports = router
   
