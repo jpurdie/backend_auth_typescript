016724164e4f0cfa38d0fad82ab8946c1df01175
Justin Purdie <justindpurdie@gmail.com>
2020-05-10 19:55:38 -0700

    'auth'

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,50 +1,38 @@
-import {
-  Entity,
-  Index,
-  PrimaryGeneratedColumn,
-  OneToMany,
-  Column,
-  CreateDateColumn,
-  UpdateDateColumn,
-  Generated
-} from 'typeorm'
-import { OrganizationUser } from './OrganizationUser'
+import { Entity, Index, PrimaryGeneratedColumn, OneToMany, Column, CreateDateColumn, UpdateDateColumn, Generated } from "typeorm";
+import { OrganizationUser } from "./OrganizationUser";
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  id: number
+  id: number;
 
   @Column()
-  @Generated('uuid')
+  @Generated("uuid")
   @Index({ unique: true })
-  uuid: string
-
-  @Index({ unique: true })
-  @Column()
-  externalId: string
-
-  @Column()
-  firstName: string
-
-  @Column()
-  lastName: string
+  uuid: string;
 
   @Index({ unique: true })
   @Column()
-  email: string
+  externalId: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Index({ unique: true })
+  @Column()
+  email: string;
 
   @CreateDateColumn()
-  createdDate: Date
+  createdDate: Date;
 
   @UpdateDateColumn()
-  updatedDate: Date
+  updatedDate: Date;
 
-  password: string
+  password: string;
 
-  @OneToMany(
-    type => OrganizationUser,
-    organizationUser => organizationUser.user
-  )
-  organizationUser: OrganizationUser[]
+  @OneToMany((type) => OrganizationUser, (organizationUser) => organizationUser.user)
+  organizationUser: OrganizationUser[];
 }
