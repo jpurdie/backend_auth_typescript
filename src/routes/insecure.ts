eab75de6782125df7fa44cf78b218424e23e774b
Justin Purdie <jdpurdie@asu.edu>
2020-05-19 21:49:57 -0700

    invitations to organization

--- src/routes/insecure.ts
+++ src/routes/insecure.ts
@@ -1,8 +1,7 @@
-const express = require('express')
-const router = express.Router()
-import  { default as PingController } from '../controllers/PingController'
+const express = require("express");
+const router = express.Router();
+import { default as PingController } from "../controllers/PingController";
 
-router.get('/ping', PingController.ping)
+router.get("/ping", PingController.ping);
 
-module.exports = router
-  
+module.exports = router;
