d584c8627224089fff268a3bd95b6a1633cb31ea
Justin Purdie <justindpurdie@gmail.com>
2021-01-26 13:11:14 -0700

    standardjs style update

--- src/controllers/PingController.ts
+++ src/controllers/PingController.ts
@@ -1,6 +1,6 @@
-import * as express from "express";
+import * as express from 'express'
 export default class PingController {
-  public static async ping(req: express.Request, res: express.Response, next) {
-    res.status(200).send("pong");
+  public static async ping (req: express.Request, res: express.Response, next) {
+    res.status(200).send('pong')
   }
 }
